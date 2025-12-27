5f78fa1a17866a59859d6b9f07f5204f47f1b74e
MovieVoice <168293827+MovieVoice@users.noreply.github.com>
2025-12-27 19:31:41 +0100

    linked id to questionaire page

--- src/pages/QuestionnairePage.jsx
+++ src/pages/QuestionnairePage.jsx
@@ -1,10 +1,13 @@
+import { useParams } from 'react-router-dom';
 import styles from './QuestionnairePage.module.css';
 
 function QuestionnairePage() {
+    const { id } = useParams();
+
     return (
-        <>
-            <div></div>
-        </>
+        <div className={styles.container}>
+            <h1>Fragebogen-Seite #{id}</h1>
+        </div>
     );
 }
 
