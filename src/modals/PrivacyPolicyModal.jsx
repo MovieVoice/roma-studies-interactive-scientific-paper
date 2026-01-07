99fc883bd1bb2d2d3c7a92750bfce9fc71c3e956
MovieVoice <168293827+MovieVoice@users.noreply.github.com>
2026-01-07 16:03:45 +0100

    updated privacy policy content

--- src/modals/PrivacyPolicyModal.jsx
+++ src/modals/PrivacyPolicyModal.jsx
@@ -25,8 +25,19 @@ function PrivacyPolicyModal() {
                             <img src="/src/assets/icons/close.svg" alt="Fenster schließen" />
                         </button>
                     </div>
-                    <div className={`${styles.content} ${styles.contentCenter}`}>
+                    <div className={styles.content}>
+                        <p className={styles.subheading}>Allgemeines</p>
+                        <div className={styles.contentSpacer}></div>
                         <p className={styles.text}>Datenschutzerklärung hinzufügen, sobald das Projekt über das Web erreichbar ist.</p>
+                        <div className={styles.contentSpacer}></div>
+                        <div className={styles.contentSpacer}></div>
+                        <p className={styles.subheading}>Icons</p>
+                        <div className={styles.contentSpacer}></div>
+                        <p className={styles.text}>Die verwendeten Icons unterliegen der Lucide License: </p>
+                        <div className={styles.contentSpacer}></div>
+                        <p className={styles.text}>ISC License</p>
+                        <p className={styles.text}>Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2023 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2025.</p>
+                        <p className={styles.text}>Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.</p>
                     </div>
                 </div>
             </div>
